94439531cd0189dd8ed52f961dc8e3621efed424
Rea-lDev <diegoasuarezl12@gmail.com>
2025-10-20 20:19:35 -0400

    Create Source.lua

---
+++ Source.lua
@@ -0,0 +1,11 @@
+{
+  "DFIntTaskSchedulerTargetFps": 240,
+  "DFFlagDebugRenderDisableTextures": true,
+  "DFFlagDebugRenderForceTechnologyVulkan": false,
+  "FFlagGlobalShadowsEnabled": false,
+  "FFlagTerrainReflectionEnabled": false,
+  "FFlagTerrainWaterReflections": false,
+  "FFlagRenderShadows": false,
+  "GraphicsQualityLevel": 1,
+  "GraphicsMode": "Manual"
+}
